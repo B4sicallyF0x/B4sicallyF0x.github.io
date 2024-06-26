c3b17eedbdb993e98a2151dee5563a3872bbeca0
B4sicallyF0x <77201486+B4sicallyF0x@users.noreply.github.com>
2024-06-27 01:02:42 +0200

    Add files via upload

--- eos/script.js
+++ eos/script.js
@@ -1,11 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById('background-music');
+    setTimeout(() => {
+        audio.play().catch(error => {
+            console.error("Playback failed due to:", error);
+        });
+    }, 1000); // Retraso de 1 segundo para garantizar que la página esté completamente cargada
+});
+
 function showMessage(language) {
     const slogan = document.getElementById('slogan');
     const audio = document.getElementById('background-music');
     if (language === 'english') {
-        slogan.textContent = 'Whoever moves first is gay.';
+        slogan.textContent = 'Whoever moves first is gay';
     } else if (language === 'spanish') {
-        slogan.textContent = 'El primero que se mueva es gay.';
+        slogan.textContent = 'El primero que se mueva es gay';
     }
     document.querySelector('.buttons').style.display = 'none';
-    audio.play(); // Inicia la reproducción del audio
+    if (audio.paused) {
+        audio.play().catch(error => {
+            console.error("Playback failed due to:", error);
+        });
+    }
 }
